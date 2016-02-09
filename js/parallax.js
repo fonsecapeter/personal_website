fbdb35373cc13c9ee84eea924cc51ff6bbfb93d2
Peter Fonseca <peter.fonseca@ucsf.edu>
2016-02-08 18:09:50 -0800

    jarvs too low on non-mobile

--- js/parallax.js
+++ js/parallax.js
@@ -6,12 +6,35 @@ window.requestAnimationFrame = window.requestAnimationFrame
   || window.msRequestAnimationFrame
   || function(f){setTimeout(f, 1000/60)}
 
+// return true if user on mobile, false if not
+function detectmob() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+
 var laptop = document.getElementById('laptop')
 var science = document.getElementById('science')
 var design = document.getElementById('design')
 var jarvs = document.getElementById('jarvs')
 //declare other objects here
 
+if ( detectmob==false ) {
+	var jarvs_top = 1000
+}
+else {
+	var jarvs_top = 500
+}
 function parallax() {
   var scrolltop = window.pageYOffset // save number of pixels scolled
 
