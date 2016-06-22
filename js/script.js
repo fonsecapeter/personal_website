c1cfca4ebcc8e563e4e6156acf659cfd60d3764e
Peter Fonseca <peter.nfonseca@gmail.com>
2016-06-21 20:35:19 -0700

    just testing single-pager without cross origin request

--- js/script.js
+++ js/script.js
@@ -1,36 +1,42 @@
 // load jQuery  when dom is loaded
 $(document).ready(function(){
 	// check url and load content accordingly (if user type #something directly into url)
-	var has = window.location.hash.substr(1);
-	var href = $('#nav a').each(function(){
-	  var href = $(this).attr('href');
-	  if(has==href.substr(0,href.length-5)){
-      var toLoad = hash+'.html #content';
-      $$('#content').load(toLoad)
+	const hash = window.location.hash.substr(1);
+	let $href = $('#nav a').each(function(){
+	  let href = $(this).attr('href');
+	  if(hash === href.substr(0,href.length-5)){
+      const toLoad = hash + '.html #content';
+      $('#content').load(toLoad);
 	  }
 	});
+
   // when menu item is clicked
-  $('#nav a').click(function(){
+  $('#nav a').click(function(event){
+		event.preventDefault();
+
     // load new content
-    var toLoad = $(this).atr('href')+' #content';
+    const toLoad = $(this).attr('href')+' #content';
     // hide old content
-    $('#content').hide('fast',loadContent);
+    $('#content').hide('fast',loadContent($(this)));
     $('#load').remove();
     // loading graphic
-    $('#wrapper').append('<span id="load">loading...</span>');
-    $('#load').fadeIn('normal');
+    // $('#wrapper').append('<span id="load">loading...</span>');
+    // $('#load').fadeIn('normal');
     // url to show #html file name
-    window.location.hsh = $(this).attr('href').substr(0,$(this).attr('href').length-5)
-  }
+    window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5);
+  });
   //load content function
-  function showNewContent(){
-    $('#content').show('normal',hideLoader);
+  // function showNewContent(){
+  function loadContent($anchor){
+		console.log($anchor.attr('href'));
+		$('#main-content').load('./' + $anchor.attr('href'));
+    // $('#content').show('normal',hideLoader);
   }
   //hide loader function
-  function hideLoader(){
-    $('#load').fadeOut('normal');
-  }
+  // function hideLoader(){
+  //   $('#load').fadeOut('normal');
+  // }
   return false;
 
-  });
+  // });
 });
