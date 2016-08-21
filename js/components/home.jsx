dd9d29d4e86b6502a12de0896c8c45ed757311e6
Peter Fonseca <peter.nfonseca@gmail.com>
2016-08-21 14:20:59 -0700

    content

--- js/components/home.jsx
+++ js/components/home.jsx
@@ -11,11 +11,11 @@ const Home = React.createClass({
           <br />
             <h2>From Science to Tech</h2>
             <p>
-              As a Clinical Research Coordinator in the field of dementia, I was positioned between administrative tasks, technical concepts, and health care. While teaching myself bash and matlab for MRI image processing/analysis, I discoverd that the same principles could apply to my own administrative tasks. I continued to learn Python and SQL to automate as much of our study's procedures as possible, freeing up enough of my time to take on new responsibilites when the senior team leader resigned.
+              As a Clinical Research Coordinator in the field of dementia, I was positioned between administrative tasks, programming concepts, and health care. While learning how to use Bash and MatLab for MRI image processing/analysis, I discoverd that the same principles could apply to the rest of my work. I continued to learn Python and SQL to automate as much of our study's procedures as possible, freeing up enough of my time to take on new responsibilites when the senior team leader resigned.
               <br /><br />
-              I grew into a role of improving our procedures with technology and am most proud of the management system I developed for our research visist summaries (see <a href="https://github.com/fonsecapeter/jarvs">Jarvs</a> in my portfolio for more information). Jarvs continues to help the study and even ended up being adopeted by another large study in the center.
+              I grew into a role of improving our procedures and data with technology and am most proud of the management system I developed for our research visist summaries (<a href="https://github.com/fonsecapeter/jarvs">Jarvs</a>).
               <br /><br />
-              While working on these projects, I realized that I my greatest services to the study came through programming. Not only did I make my co-workers lives easier, I was helping our research participants recieve better care. I enjoyed this work more than my other responsibilities and decided that I wanted to do it every day. My goal is to continue learning how I can leverage technology to help others.
+              While working on these projects, I realized that my greatest services to the study came through programming. Not only was I making my co-workers lives easier, I was helping our research participants recieve better care. I enjoyed this work more than my other responsibilities and decided that I wanted to do it every day. My goal is to continue learning how I can leverage technology to help others.
               <br /><br />
             </p>
           </div>
