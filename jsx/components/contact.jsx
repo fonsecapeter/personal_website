4ccd348cbc7f84212a4ae9b371ad61b80fc1a8d8
Peter Fonseca <pfonseca@23andme.com>
2017-11-01 12:48:35 -0700

    Rework webpack pipeline
    Also:
      - temporarily stub elevator pitch
      - remove jarvs (pretty old... make room for more later)
      - standardize item padding to 25px
      - make the map a card top/bottom

--- jsx/components/contact.jsx
+++ jsx/components/contact.jsx
@@ -52,7 +52,6 @@ const Contact = React.createClass ({
             </p>
             <p>San Francisco, CA</p>
           </div>
-          <br/><br />
           <div id="map" className="contact-map" style={{overflow:"hidden", minHeight:"360px", minWidth:"42px"}}></div>
           <script async defer
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBnDvU2SC_NgC7XymGmvT03_oNPIl1SsC4&callback=initMap">
