3374c0a8a61eb8482e2aaf2919afaffb6dbba1ba
Peter Fonseca <peter.nfonseca@gmail.com>
2017-12-21 19:59:24 -0800

    New picture!
    Minor enhancements

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,4 @@ module.exports = {
   },
 
   extends: 'airbnb',
-
-  // Always enforce rules with 'error'. We want the build to break on style violations.
-  rules: {
-    // Rules we're deliberately changing
-    'func-names': 'off',
-    'indent': ['error', 2],
-    'no-underscore-dangle': 'off',
-    'quote-props': 'off',
-    'react/jsx-indent': ['error', 2],
-    'react/jsx-indent-props': ['error', 2],
-    'jsx-a11y/no-static-element-interactions': 'off',
-    'import/extensions': ['off', 'never'],
-  },
 };
