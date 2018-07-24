d61f7431fb25379a5f0aaf6e1d308694b6f15acb
Peter Fonseca <pfonseca@23andme.com>
2018-07-24 09:42:36 -0700

    Forget map ever happened

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -5,18 +5,11 @@ class Contact extends React.Component<any, any> {
     return (
       <div>
         <h1 className="contact-title">Contact Me</h1>
-        <div className="contact">
-          <div className="contact-info">
-            <p>
-              <a href="mailto:peter.nfonseca@gmail.com">peter.nfonseca@gmail.com</a>
-            </p>
-            <p className="contact-info-city">San Francisco, CA</p>
-          </div>
-          <div
-            id="map"
-            className="contact-map"
-            style={{ overflow: 'hidden', minHeight: '360px', minWidth: '42px' }}
-          />
+        <div className="contact-info">
+          <p>
+            <a href="mailto:peter.nfonseca@gmail.com">peter.nfonseca@gmail.com</a>
+          </p>
+          <p className="contact-info-city">San Francisco, CA</p>
         </div>
       </div>
     );
