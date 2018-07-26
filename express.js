51094e930dd0e0cc706a151d46d5a42be5377276
Peter Fonseca <pfonseca@23andme.com>
2018-07-26 13:12:03 -0700

    Add status route to keep alive in heroku

--- express.js
+++ express.js
@@ -3,11 +3,15 @@ const express = require('express');
 const compression = require('compression'); // gzip
 
 const app = express();
-app.use(compression());
 const portNumber = process.env.PORT || 3000;
 const sourceDir = 'dist';
 
+app.use(compression());
 app.use(express.static(sourceDir));
+
+app.get('/_status', (req, res) => {
+  res.send('Status: OK');
+});
 
 app.listen(portNumber, () => {
   console.log(`Express web server started: http://localhost:${portNumber}`);
