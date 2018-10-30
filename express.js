0af313fac6a7b184a6e0a18b5fd57f1d43bef908
Peter Fonseca <pfonseca@23andme.com>
2018-10-30 11:56:25 -0700

    Delete ssl cert
    Certs appear to be no good, just removing everything

--- express.js
+++ express.js
@@ -5,17 +5,12 @@ const compression = require('compression'); // gzip
 const app = express();
 const portNumber = process.env.PORT || 3000;
 const sourceDir = 'dist';
-const cert = process.env.CERT;
 
 app.use(compression());
 app.use(express.static(sourceDir));
 
 app.get('/_status', (req, res) => {
   res.send('Status: OK');
-});
-
-app.get('/.well-known/acme-challenge/:content', (req, res) => {
-  res.send(cert);
 });
 
 app.listen(portNumber, () => {
