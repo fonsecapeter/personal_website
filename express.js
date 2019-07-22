b00b2575be1aacfa0f2d82b1a086bbc2eac5cb6a
Peter Fonseca <pfonseca@23andme.com>
2019-07-22 11:37:37 -0700

    Enable http://wakemydyno.com

--- express.js
+++ express.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV === 'production') require('newrelic');
 const express = require('express');
 const compression = require('compression'); // gzip
+const path = require('path');
 
 const app = express();
 const portNumber = process.env.PORT || 3000;
@@ -11,6 +12,10 @@ app.use(express.static(sourceDir));
 
 app.get('/_status', (req, res) => {
   res.send('Status: OK');
+});
+
+app.get('/wakemydyno.txt', (req, res) => {
+  res.sendFile(path.join(__dirname, 'wakemydyno.txt'));
 });
 
 app.listen(portNumber, () => {
