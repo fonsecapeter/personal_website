b902031b97a6bca7abea13b118903b3a6d8528fe
Peter Fonseca <peter.nfonseca@gmail.com>
2023-10-16 11:46:50 -0700

    Move neuropy down

--- src/content/portfolio/code.ts
+++ src/content/portfolio/code.ts
@@ -69,29 +69,6 @@ export const CODE: Project[] = [
   },
 
   {
-    id: 'neuropy',
-    name: 'Brain MRI Deep Learning',
-    links: [
-      {
-        text: 'github',
-        url: 'https://github.com/fonsecapeter/neuropy',
-      },
-    ],
-    date: '2017',
-    keywords: 'deep learning exploration',
-    icon: {
-      src: brain,
-    },
-    description: 'When I first learned how accessible tensor flow was, I had to see if I could hack together a quick deep learning model trained to diagnose Alzheimer\'s from 3D brain MRI scans. The models themselves are faily simple, but the intersting part is the pipeline I built to feed them data. This involved loading sample MRI scans from public sources (including a single MRI of my own brain) and the steps to normalize and process them so that they could be fed into tensorflow (including quality checks along the way). At the time, I could only find one paper on training models against MRI data so adapted what I learned from UCSF about volumetric studies to internet guides for image recognition models to see if I could replicate something similar. I quickly ran into scaling limitations so decided to try any other public, labelled brain MRI datasets for similar classification studies that I could find. I stopped at the point where I\'d need some sort of research partnership to scale up for more data, but did lay the groundwork for something that could be expanded and tuned.',
-    bullets: [
-      'Convolutional neural nets',
-      'Leveraged a public datasets',
-      'Complete data pipeline from raw MRI exports',
-      'Voxels',
-    ],
-  },
-
-  {
     id: 'api-buddy',
     name: 'API Buddy',
     links: [
@@ -222,6 +199,29 @@ export const CODE: Project[] = [
     },
     description: 'Led a team of engineers to build an API integration with a 3rd party mobile app that helps customers manage chronic diseases.',
     bullets: [],
+  },
+
+  {
+    id: 'neuropy',
+    name: 'Brain MRI Deep Learning',
+    links: [
+      {
+        text: 'github',
+        url: 'https://github.com/fonsecapeter/neuropy',
+      },
+    ],
+    date: '2017',
+    keywords: 'deep learning exploration',
+    icon: {
+      src: brain,
+    },
+    description: 'When I first learned how accessible tensor flow was, I had to see if I could hack together a quick deep learning model trained to diagnose Alzheimer\'s from 3D brain MRI scans. The models themselves are faily simple, but the intersting part is the pipeline I built to feed them data. This involved loading sample MRI scans from public sources (including a single MRI of my own brain) and the steps to normalize and process them so that they could be fed into tensorflow (including quality checks along the way). At the time, I could only find one paper on training models against MRI data so adapted what I learned from UCSF about volumetric studies to internet guides for image recognition models to see if I could replicate something similar. I quickly ran into scaling limitations so decided to try any other public, labelled brain MRI datasets for similar classification studies that I could find. I stopped at the point where I\'d need some sort of research partnership to scale up for more data, but did lay the groundwork for something that could be expanded and tuned.',
+    bullets: [
+      'Convolutional neural nets',
+      'Leveraged a public datasets',
+      'Complete data pipeline from raw MRI exports',
+      'Voxels',
+    ],
   },
 
   {
