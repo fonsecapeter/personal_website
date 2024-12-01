c95e63f460ca2bbb4c0cf1878af665cb549d0b10
Peter Fonseca <peter.nfonseca@gmail.com>
2024-11-30 17:02:08 -0800

    Fix type annotation

--- src/content/image.ts
+++ src/content/image.ts
@@ -7,7 +7,7 @@ export type Image = {
   readonly alt: string,
 };
 
-export const image = (path: string, ext: str, alt: string): Image => ({
+export const image = (path: string, ext: string, alt: string): Image => ({
   // full: `${prefix}${path}/100.${ext} => ${mainImage}`,
   full: require(`../assets/img/${path}/100.${ext}`).default,
   half: require(`../assets/img/${path}/50.${ext}`).default,
