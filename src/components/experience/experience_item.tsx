c5cb76205e3d26655e281b86e8508ac04d88ec48
Peter Fonseca <peter.nfonseca@gmail.com>
2024-12-03 13:27:45 -0800

    Resolve #33 All functional components
    Also bump react from 16 -> 17

--- src/components/experience/experience_item.tsx
+++ src/components/experience/experience_item.tsx
@@ -1,132 +1,98 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React, { Component, KeyboardEvent } from 'react';
+import React, { useState, KeyboardEvent } from 'react';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Experience } from '../../content/experiences';
 
 type ExperienceProps = {
   readonly experience: Experience,
 };
-type ExperienceState = {
-  hidden: boolean,
-  expandClass: string,
-};
 
-const expandState: ExperienceState = {
-  hidden: false,
-  expandClass: 'experience-item-bullets',
-};
-const hideState: ExperienceState = {
-  hidden: true,
-  expandClass: 'hidden',
-};
+export const ExperienceItem = ({ experience }: ExperienceProps) => {
+  const [hidden, setHidden] = useState(false);
 
-export class ExperienceItem extends Component<ExperienceProps, ExperienceState> {
-  readonly state = expandState;
-
-  constructor(props: ExperienceProps) {
-    super(props);
-    this.expand = this.expand.bind(this);
-    this.hide = this.hide.bind(this);
-    this.toggleDrop = this.toggleDrop.bind(this);
-    this.toggleDropOnEnter = this.toggleDropOnEnter.bind(this);
-  }
-
-  private expand() {
-    this.setState(expandState);
-  }
-
-  private hide() {
-    this.setState(hideState);
-  }
-
-  private toggleDrop() {
-    const { experience } = this.props;
-    const { hidden } = this.state;
+  const toggleDrop = () => {
     if (experience.bullets.length > 0) {
       if (hidden) {
-        this.expand();
+        setHidden(false);
       } else {
-        this.hide();
+        setHidden(true);
       }
     }
-  }
+  };
 
-  private toggleDropOnEnter(event: KeyboardEvent) {
+  const toggleDropOnEnter = (event: KeyboardEvent) => {
     const code = event.keyCode || event.charCode;
     if (code === 13) {
       event.preventDefault();
       event.stopPropagation();
-      this.toggleDrop();
+      toggleDrop();
     }
+  };
+
+  const expandClass = hidden ? 'hidden' : 'experience-item-bullets';
+  let primary = null;
+  if (experience.primary) {
+    primary = <p>{experience.primary}</p>;
   }
 
-  public render() {
-    const { experience } = this.props;
-    const { expandClass } = this.state;
-    let primary = null;
-    if (experience.primary) {
-      primary = <p>{experience.primary}</p>;
-    }
+  let action: (() => void) | null = null;
+  let actionOnEnter: ((event: KeyboardEvent) => void) | null = null;
+  let experienceItemClassName = 'experience-item';
+  let tabIndex = -1;
+  if (experience.bullets.length > 0) {
+    action = () => toggleDrop();
+    actionOnEnter = (event) => toggleDropOnEnter(event);
+    experienceItemClassName += '-interactive';
+    tabIndex = 0;
+  }
 
-    let action = null;
-    let actionOnEnter = null;
-    let experienceItemClassName = 'experience-item';
-    let tabIndex = -1;
-    if (experience.bullets.length > 0) {
-      action = this.toggleDrop;
-      actionOnEnter = this.toggleDropOnEnter;
-      experienceItemClassName += '-interactive';
-      tabIndex = 0;
-    }
-
-    return (
-      <div
-        className={experienceItemClassName}
-        onClick={action}
-        onKeyDown={actionOnEnter}
-        role="button"
-        tabIndex={tabIndex}
-      >
-        <div className="experience-item-content">
-          <div className="experience-item-left">
-            <img
-              src={experience.icon}
-              className="experience-item-icon"
-              alt="icon"
-            />
-          </div>
-          <div className="experience-item-right">
-            <h3 className="experience-item-title">
-              {experience.title}
-            </h3>
-            <p>
-              <span className="experience-item-date">
-                {experience.date}
-              </span>
-              <span className="experience-item-dot">·</span>
-              <a
-                className="experience-item-org"
-                href={experience.link}
-                target="blank"
-                rel="noopener noreferrer"
-              >
-                {experience.org}
-              </a>
-            </p>
-            {primary}
-            <div>
-              <ul className={expandClass}>
-                {experience.bullets.map(bullet => (
-                  <li
-                    key={bullet}
-                    dangerouslySetInnerHTML={{ __html: bullet }}
-                  />
-                ))}
-              </ul>
-            </div>
+  return (
+    <div
+      className={experienceItemClassName}
+      onClick={action}
+      onKeyDown={actionOnEnter}
+      role="button"
+      tabIndex={tabIndex}
+    >
+      <div className="experience-item-content">
+        <div className="experience-item-left">
+          <img
+            src={experience.icon}
+            className="experience-item-icon"
+            alt="icon"
+          />
+        </div>
+        <div className="experience-item-right">
+          <h3 className="experience-item-title">
+            {experience.title}
+          </h3>
+          <p>
+            <span className="experience-item-date">
+              {experience.date}
+            </span>
+            <span className="experience-item-dot">·</span>
+            <a
+              className="experience-item-org"
+              href={experience.link}
+              target="blank"
+              rel="noopener noreferrer"
+            >
+              {experience.org}
+            </a>
+          </p>
+          {primary}
+          <div>
+            <ul className={expandClass}>
+              {experience.bullets.map(bullet => (
+                <li
+                  key={bullet}
+                  dangerouslySetInnerHTML={{ __html: bullet }}
+                />
+              ))}
+            </ul>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,142 +1,27 @@
-import React, { Component } from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { NavLink } from './nav_link';
 
-type NavState = {
-  selected: string | null,
+const NavBase = () => {
+  const path = window.location.hash.replace('#/', '');
+  const scrollToTop = () => window.scrollTo(0, 0);
+  const ABOUT = 'about';
+  const EXPERIENCE = 'experience';
+  const CODE = 'code';
+  const ART = 'art';
+  const links = [ABOUT, EXPERIENCE, CODE, ART];
+  return (
+    <nav id="nav" className="nav">
+      {links.map((link) => (
+        <NavLink
+          name={link}
+          active={path === link}
+          onClick={scrollToTop}
+          key={link}
+        />
+      ))}
+    </nav>
+  );
 };
-type NavLinkSpec = {
-  readonly name: string,
-  readonly select: () => void,
-  readonly active: boolean,
-};
-
-const ABOUT = 'about';
-const EXPERIENCE = 'experience';
-const CODE = 'code';
-const ART = 'art';
-
-const initialState: NavState = {
-  selected: ABOUT,
-};
-const aboutState: NavState = { selected: ABOUT };
-const experienceState: NavState = { selected: EXPERIENCE };
-const codeState: NavState = { selected: CODE };
-const artState: NavState = { selected: ART };
-const clearedState: NavState = { selected: null };
-const scrollToTop = () => window.scrollTo(0, 0);
-
-class NavBase extends Component<RouteComponentProps, NavState> {
-  readonly state = initialState;
-
-  unlisten = null; // set in componentWillMount
-
-  constructor(props: RouteComponentProps) {
-    super(props);
-    this.selectLink = this.selectLink.bind(this);
-    this.selectAbout = this.selectAbout.bind(this);
-    this.selectExperience = this.selectExperience.bind(this);
-    this.selectCode = this.selectCode.bind(this);
-    this.selectArt = this.selectArt.bind(this);
-  }
-
-  componentWillMount() {
-    const { history } = this.props;
-    this.selectLink();
-    this.unlisten = history.listen(() => {
-      this.selectLink();
-    });
-  }
-
-  componentWillUnmount() {
-    this.unlisten();
-  }
-
-  private selectLink() {
-    // Select from url hash fragment because there is not history location on initial load
-    const path = window.location.hash.replace('#/', '');
-    switch (path) {
-      case ABOUT:
-        this.selectAbout();
-        break;
-      case EXPERIENCE:
-        this.selectExperience();
-        break;
-      case CODE:
-        this.selectCode();
-        break;
-      case ART:
-        this.selectArt();
-        break;
-      case '':
-        this.selectAbout();
-        break;
-      default:
-        this.clear();
-    }
-  }
-
-  private clear() {
-    this.setState(clearedState);
-  }
-
-  private selectAbout() {
-    scrollToTop();
-    this.setState(aboutState);
-  }
-
-  private selectExperience() {
-    scrollToTop();
-    this.setState(experienceState);
-  }
-
-  private selectCode() {
-    scrollToTop();
-    this.setState(codeState);
-  }
-
-  private selectArt() {
-    scrollToTop();
-    this.setState(artState);
-  }
-
-  public render() {
-    const { selected } = this.state;
-    const links: NavLinkSpec[] = [
-      {
-        name: ABOUT,
-        select: this.selectAbout,
-        active: selected === ABOUT,
-      },
-      {
-        name: EXPERIENCE,
-        select: this.selectExperience,
-        active: selected === EXPERIENCE,
-      },
-      {
-        name: CODE,
-        select: this.selectCode,
-        active: selected === CODE,
-      },
-      {
-        name: ART,
-        select: this.selectArt,
-        active: selected === ART,
-      },
-    ];
-    return (
-      <nav id="nav" className={`nav nav-${selected}`}>
-        {links.map((link) => (
-          <NavLink
-            name={link.name}
-            active={link.active}
-            onClick={() => link.select()}
-            key={link.name}
-          />
-        ))}
-      </nav>
-    );
-  }
-}
 
 export const Nav = withRouter(NavBase);
