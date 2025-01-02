b5e1546eeb72a6e1a94c586e7d3d8d852ddf17b7
Peter Fonseca <peter.nfonseca@gmail.com>
2025-01-02 15:17:04 -0800

    Use carousel for brass mono

--- src/content/portfolio/code_projects/brass_mono.ts
+++ src/content/portfolio/code_projects/brass_mono.ts
@@ -1,8 +1,14 @@
 import { Project } from '../projects';
 
-import brassMono100 from '../../../assets/img/portfolio/brass_mono/100.png';
-import brassMono50 from '../../../assets/img/portfolio/brass_mono/50.png';
-import brassMono25 from '../../../assets/img/portfolio/brass_mono/25.png';
+import main100 from '../../../assets/img/portfolio/brass_mono/main/100.png';
+import main50 from '../../../assets/img/portfolio/brass_mono/main/50.png';
+import main25 from '../../../assets/img/portfolio/brass_mono/main/25.png';
+import firstWild100 from '../../../assets/img/portfolio/brass_mono/in_the_wild_1/100.jpg';
+import firstWild50 from '../../../assets/img/portfolio/brass_mono/in_the_wild_1/50.jpg';
+import firstWild25 from '../../../assets/img/portfolio/brass_mono/in_the_wild_1/25.jpg';
+import secondWild100 from '../../../assets/img/portfolio/brass_mono/in_the_wild_2/100.jpg';
+import secondWild50 from '../../../assets/img/portfolio/brass_mono/in_the_wild_2/50.jpg';
+import secondWild25 from '../../../assets/img/portfolio/brass_mono/in_the_wild_2/25.jpg';
 
 
 const BRASS_MONO: Project = {
@@ -21,18 +27,30 @@ const BRASS_MONO: Project = {
   date: '2017',
   keywords: 'open source font',
   icon: {
-    src: brassMono50,
+    src: main50,
     alt: 'letters and electronics',
   },
   images: [
     {
-      full: brassMono100,
-      half: brassMono50,
-      quarter: brassMono25,
-      alt: 'letters and electronics',
+      full: main100,
+      half: main50,
+      quarter: main25,
+      alt: 'letters and cartoon electronics in the brass mono font',
+    },
+    {
+      full: firstWild100,
+      half: firstWild50,
+      quarter: firstWild25,
+      alt: 'example brass mono inspiration collage',
+    },
+    {
+      full: secondWild100,
+      half: secondWild50,
+      quarter: secondWild25,
+      alt: 'another example brass mono inspiration collage',
     },
   ],
-  description: 'Retro monospaced font inspired by 70\'s electrical and mechanical design design. It\'s the only font I use on this website and write code in it every day.',
+  description: 'A free retro monospaced font inspired by 20th century electrical and mechanical design. It\'s the only font I use on this website and I write code in it every day.',
   bullets: [
     'Docker',
     'Automated regular / bold / italic build',
