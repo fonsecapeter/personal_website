33b3aba2c110fff2f38b6aa336c153a8c2af97dd
Peter Fonseca <peter.nfonseca@gmail.com>
2025-01-12 08:15:50 -0800

    Update api-buddy

--- src/content/portfolio/code_projects/api_buddy.ts
+++ src/content/portfolio/code_projects/api_buddy.ts
@@ -1,8 +1,6 @@
 import { Project } from '../projects';
 
 import apiBuddy100 from '../../../assets/img/portfolio/api_buddy/100.gif';
-import apiBuddy50 from '../../../assets/img/portfolio/api_buddy/50.gif';
-import apiBuddy25 from '../../../assets/img/portfolio/api_buddy/25.gif';
 
 
 const API_BUDDY: Project = {
@@ -21,22 +19,22 @@ const API_BUDDY: Project = {
   date: '2019',
   keywords: 'python cli tool',
   icon: {
-    src: apiBuddy50,
+    src: apiBuddy100,
     alt: 'terminal with api call',
   },
   images: [
     {
       full: apiBuddy100,
-      half: apiBuddy50,
-      quarter: apiBuddy25,
+      half: apiBuddy100,  // doesn't look good low-res, just use 100 everywhere
+      quarter: apiBuddy100,
       alt: 'animated gif of terminal making api call',
     },
   ],
-  description: 'CLI tool for exploring APIs. I Use this all the time at work. Postman is great, but it\'s also complicated and takes too long to set up when I want an answer right away. This has is exactly what I need for working with API\'s, nothing less, nothing more.',
+  description: 'CLI tool for exploring APIs. I Use this all the time at work. Postman is great, but it\'s also fully feature and takes too long to set up when I want an answer right away. This has is exactly what I need for working with API\'s, nothing more, nothing less.',
   bullets: [
-    'Intuitive interface',
+    'Pleasant, intuitive interface',
     'Automatic OAuth2 authentication',
-    'Pleasant text-based UI/UX',
+    '100% code coverage',
   ],
 };
 
